b9bd563044e1811a044b5c9085dd9b1a9e929fb8
leoartesuave <113612294+leoartesuave@users.noreply.github.com>
2023-02-03 10:12:23 -0300

    praticas de logicas

---
+++ javaestudos/menor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class menor {
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+System.out.println("um numero: ");
+int num = entrada.nextInt();
+
+System.out. println("outro numero: ");
+int num2 = entrada.nextInt();
+
+if(num<num2){
+
+    System.out.println("o menor e: " + num);
+ 
+}else if (num>num2){
+    System.out.println("o menor e: " + num2);
+}
+else{
+    System.out.println("sao iguais!!");
+}
+
+
+    }
+    }
